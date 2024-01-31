0e1734394ef02f6950ec27962e5cfb0523e38ee0
Jiachi Liu <liu3878@data.cs.purdue.edu>
2024-01-31 09:00:24 -0500

    Done!

---
+++ answer.sh
@@ -0,0 +1,14 @@
+mv Homework/MA165/HW2.txt HW1.txt
+rm Memes/SpongeBob/money.jpeg 
+mv Star Wars/fafsa.jpg Memes/SpongeBob
+mv Homework/CS191
+touch Programs/C++
+mv Programs/Age.cpp Programs/C++
+cp Programs/Java Homework/CS180/Project_1
+mv Programs/Count.c Programs/C 
+mv Programs/Goodbye.c Programs/C
+mv Programs/Name.py Programs/Python 3
+mv Programs/Space.py Programs/Python 3
+touch Homework/POL141 Essay.txt
+ls
+
